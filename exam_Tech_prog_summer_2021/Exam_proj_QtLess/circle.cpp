406c0245f9dc42dac36668c607d327c5b2c9e33e
ZadnepryanskayaAlicia <71925051+ZadnepryanskayaAlicia@users.noreply.github.com>
2021-07-03 12:57:46 +0300

    exam_Tech_prog_summer_2021

---
+++ exam_Tech_prog_summer_2021/Exam_proj_QtLess/circle.cpp
@@ -0,0 +1,13 @@
+#include "circle.h"
+
+circle::circle() //конструктор
+{
+
+}
+//функция для круга с параметрами x0, y0, r
+circle::circle(double x, double y,double rad)
+{
+    x0=x; //вершина x
+    y0=y; //вершина y
+    r=rad; //радиус
+}
---
+++ exam_Tech_prog_summer_2021/Exam_proj_QtLess/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "triangle.h"
+#include "square.h"
+#include "circle.h"
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(int argc,char*argv[])
+{
+    cout<<"Hello, " << endl;  //Приветственное сообщение для пользователя
+
+    double qwe[8]={0,0,1,0,1,1,0,1};
+    square p = square(qwe);
+    if (p.figure_square())
+        cout <<"Square" << endl;
+
+    return 0;
+}
