8e27628cf606092d52f64ee7cf282a5b675aac52
Natalia <61887532+NataliaDemyanova@users.noreply.github.com>
2021-07-03 12:35:00 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/**
+* \file
+* \brief main файл проекта.
+*/
+#include <iostream>
+#include "square.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+
+int x;
+
+cin >> x;
+
+cout << "P=" << x * 4;
+
+return 0;
+
+}
---
+++ square.cpp
@@ -0,0 +1,27 @@
+/**
+* \file
+* \brief файл реализации методов класса Square.
+*/
+#include "square.h"
+/**
+ * @brief square::square конструктор класса
+ */
+square::square()
+{
+
+}
+/**
+ * @brief square::square конструктор класса через массив координат.
+ * @param ver - массив, содержащий double значения координат вершин
+ */
+square::square(double ver[])
+{
+    a_x = ver[0];
+    a_y = ver[1];
+    b_x = ver[2];
+    b_y = ver[3];
+    c_x = ver[4];
+    c_y = ver[5];
+    d_x = ver[6];
+    d_y = ver[7];
+}
