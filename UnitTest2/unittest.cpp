c9643fb647502e60604f968e9dd64ea625fff43b
Kertny <gosha_mat@mail.ru>
2021-07-03 13:19:28 +0300

    unit

---
+++ UnitTest2/unittest.cpp
@@ -0,0 +1,29 @@
+#include <QtTest/QtTest>
+#include <iostream>
+
+#include "E:\Exam\exam_Tech_prog_summer_2021\Exam_proj_QtLess\triangle.h"
+
+class UnitTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void test_case1();
+};
+
+
+/*!
+ * \brief UnitTest::test_case1 Unit Test для проверки метода вычисления площади треугольника
+ */
+
+void UnitTest::test_case1()
+{
+    double mas[6] = {2,3,5,8,8};
+    triangle *tri = new triangle(mas);
+    double result = tri->perimeter();
+    std::cout << "Вывод: " << result;
+    QVERIFY(result == 15.0);
+}
+
+QTEST_MAIN(UnitTest)
+#include "unittest.moc"
