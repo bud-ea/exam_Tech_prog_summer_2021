7028c9c034ee4ed769138a8e28bccc09e49dd35f
v279e <71925316+v279e@users.noreply.github.com>
2021-07-03 13:06:57 +0300

    Add files via upload

--- Exam_proj_QtLess/square.cpp
+++ Exam_proj_QtLess/square.cpp
@@ -16,3 +16,18 @@ square::square(double ver[])
     d_x = ver[6];
     d_y = ver[7];
 }
+
+double square::figure_square()
+{
+    ay=b_y - a_y;
+    ax=b_x - a_x;
+    a = sqrt(ax*ax+ay*ay);
+    bx=c_x - b_x;
+    by=c_y - b_y;
+    b = sqrt(bx*bx+by*by);
+
+    if (a==b, a_x==b_x==c_x==d_x)
+        return true;
+    else
+        return false;
+}
