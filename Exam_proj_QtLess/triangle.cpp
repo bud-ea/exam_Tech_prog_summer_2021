e4229cb7f6ef0a9c7ff2c3f160f0327bc07ac4ed
VioTerra <72231969+VioTerra@users.noreply.github.com>
2021-07-03 13:00:23 +0300

    Add files via upload

--- Exam_proj_QtLess/triangle.cpp
+++ Exam_proj_QtLess/triangle.cpp
@@ -1,11 +1,11 @@
 #include "triangle.h"
 
-triangle::triangle()
+triangle::triangle() // constructor
 {
 
 }
 
-triangle::triangle(double ver[])
+triangle::triangle(double ver[]) // constructor s ispolzovaniem massiva
 {
     a_x = ver[0];
     a_y = ver[1];
@@ -13,4 +13,19 @@ triangle::triangle(double ver[])
     b_y = ver[3];
     c_x = ver[4];
     c_y = ver[5];
+}
+
+triangle::area() // nahozhdenie ploshadi
+{
+    double storona_ab = sqrt(pow(b_x - a_x, 2) + pow(b_y - a_y, 2));
+
+    double storona_bc = sqrt(pow(c_x - b_x, 2) + pow(c_y - b_y, 2));
+
+    double storona_ac = sqrt(pow(c_x - a_x, 2) + pow(c_y - a_y, 2));
+
+    double halfperim = (storona_ab + storona_bc + storona_ac) / 2;
+
+    double area = sqrt(halfperim * (halfperim - storona_ab) * (halfperim - storona_bc) * (halfperim - storona_ac));
+    
+    return area;
 }
--- Exam_proj_QtLess/triangle.h
+++ Exam_proj_QtLess/triangle.h
@@ -5,10 +5,11 @@
 class triangle
 {
 public:
-    triangle();
-    triangle(double ver[]);
+    triangle(); // constructor
+    triangle(double ver[]); // constructor s ispolzovaniem massiva
+    area(); // nahozhedenie ploshadi
 private:
-    double a_x, a_y, b_x, b_y, c_x, c_y;
+    double a_x, a_y, b_x, b_y, c_x, c_y; // x and y dlya kashdoi storoni
 };
 
 #endif // TRIANGLE_H
