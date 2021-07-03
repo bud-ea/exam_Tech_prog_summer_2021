54ed51d416db8a3865e5ae228f408628979365df
Логачева Анна <72224593+AnnLogacheva@users.noreply.github.com>
2021-07-03 13:09:18 +0300

    Add files via upload

---
+++ Documents/Exam/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "triangle.h"
+#include <math.h>
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(int argc,char*argv[])
+{
+    cout<<"Hello, "<< argv[1] << endl;
+
+    cout <<"Do you want to " <<  " find the perimeter of the traingle?" << endl;
+
+    cout <<"Entet the coordinates of the points (x,y)*3"<< endl;
+
+    int i=0;
+    while (i != 1)
+    {
+        triangle::triangle(double x);
+        i+=1;
+    };
+
+    getchar();
+    return 0;
+}
