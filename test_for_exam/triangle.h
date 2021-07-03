f08899c2b7332cf4d2633e6d87d197360dc0c7a9
andreybesedin <becedingenrix@gmail.com>
2021-07-03 19:06:53 +0300

    first

---
+++ test_for_exam/triangle.h
@@ -0,0 +1,15 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+
+class triangle
+{
+public:
+    triangle();
+    triangle(double ver[]);
+    double Perimeter();
+private:
+    double a_x, a_y, b_x, b_y, c_x, c_y;
+};
+
+#endif // TRIANGLE_H
---
+++ test_for_exam/tst_my_tests.cpp
@@ -0,0 +1,50 @@
+#include <QtTest>
+#include"/Users/andreybesedin/Downloads/exam_Tech_prog_summer_2021-master/Exam_proj_QtLess/triangle.h"
+// add necessary includes here
+
+class my_tests : public QObject
+{
+    Q_OBJECT
+
+public:
+    my_tests();
+    ~my_tests();
+
+private slots:
+    void test_case1();
+    void test_case2();
+    void test_case3();
+
+};
+
+my_tests::my_tests()
+{
+
+}
+
+my_tests::~my_tests()
+{
+
+}
+
+void my_tests::test_case1()
+{
+    double t[6]={0,0,0,4,3,0};
+    triangle n = triangle(t);
+    QVERIFY2(n.Perimeter()==12,"incorrect");
+}
+void my_tests::test_case2()
+{
+    double t[6]={1,2,3,4,5,6};
+    triangle n = triangle(t);
+QVERIFY2(n.Perimeter()==5,"incorrect");
+}
+void my_tests::test_case3()
+{
+    double t[6]={1,2,3,4,5,6};
+    triangle n = triangle(t);
+    QVERIFY2(n.Perimeter()==43,"incorrect");
+}
+QTEST_APPLESS_MAIN(my_tests)
+
+#include "tst_my_tests.moc"
