a34d04da3c131504d810995b704b693e1bb2bcf4
ZadnepryanskayaAlicia <71925051+ZadnepryanskayaAlicia@users.noreply.github.com>
2021-07-03 12:59:45 +0300

    exam res

---
+++ документация/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "cst",
+  1: "cst"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
