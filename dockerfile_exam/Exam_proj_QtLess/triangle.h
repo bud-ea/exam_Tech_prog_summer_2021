4491e421e861755f8a8dfa4654a3f23b5b1b4ff0
ZadnepryanskayaAlicia <71925051+ZadnepryanskayaAlicia@users.noreply.github.com>
2021-07-03 12:56:50 +0300

    dockerfile_exam

---
+++ dockerfile_exam/Exam_proj_QtLess/triangle.h
@@ -0,0 +1,14 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+
+class triangle
+{
+public:
+    triangle(); //конструктор
+    triangle(double ver[]);
+private:
+    double a_x, a_y, b_x, b_y, c_x, c_y;
+};
+
+#endif // TRIANGLE_H
