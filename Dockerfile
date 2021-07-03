d972a2550b787ba0072b79828007114cd5987e75
Константин Рябов <K.D.Riabov@stud.mospolytech.ru>
2021-07-03 12:15:31 +0300

    Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM ubuntu:latest
+LABEL maintainer="Riabov_201-351"
+
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt update
+RUN apt -y install g++
+
+ADD Exam_proj_QtLess .
+
+RUN g++ -o myProgram main.cpp square.cpp triangle.cpp circle.cpp
+CMD ["./myProgram", "Riabov_201-351"]
--- Exam_proj_QtLess/main.cpp
+++ Exam_proj_QtLess/main.cpp
@@ -13,10 +13,10 @@ int main(int argc,char*argv[])
     cout<<"Hello, "<< argv[1] << endl;
 
     cout <<"Do you want to " <<  "find triangle area?" << endl;
-
+    cout <<"Vert {2, -3, 1, 1, -6, 5}" << endl;
     double a[] = {2, -3, 1, 1, -6, 5};
     triangle tr(a);
-    cout << "Triangle area is" << tr.area() << endl;
+    cout << "Triangle area is " << tr.area() << endl;
     /* your code here*/
 
     return 0;
