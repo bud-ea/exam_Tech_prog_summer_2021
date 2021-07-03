59848935b8147b79ff3289fae8e85362568b9a63
rogozhin-ea <flaimerofficial@yandex.ru>
2021-07-03 11:48:11 +0300

    Add

--- Exam_proj_QtLess/main.cpp
+++ Exam_proj_QtLess/main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
+#include <cmath> ///подключаем библиотеку для вычисления модуля
 #include "triangle.h"
-#include "square.h"
-#include "circle.h"
-
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-int main(int argc,char*argv[])
+int main(int argc, char* argv[])
 {
-    cout<<"Hello, "<< argv[1] << endl;
+    cout << "Hello" << endl;
+    cout << "Do you want to " << "your task" << endl;
+    cout << "Enter the coordinates of the vertices of the triangle" << endl;
+    cin >> a_x >> a_y >> b_x >> b_y >> c_x >> c_y; ///вводим координаты всех точек треугольника
 
-    cout <<"Do you want to " <<  "your task" << endl;
-
-    /* your code here*/
+    cout << abs((b_x - a_x) * (c_y - a_y) - (c_x - a_x) * (b_y - a_y)) / 2 << endl; ///вычисляем площадь треугольника по формуле
 
     return 0;
 }
