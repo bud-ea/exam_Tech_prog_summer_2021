745cae8a53f064d06519e8624f187612ac1a254e
Pavlov Kirill <72216377+Ch1lln1ght@users.noreply.github.com>
2021-07-03 13:00:13 +0300

    added Unit tests

---
+++ Exam_proj_QtLess/square.cpp
@@ -0,0 +1,49 @@
+#include "square.h"
+
+/*!
+ * \brief square::square - конструктор класса square без параметров
+ */
+square::square() {
+
+}
+
+/*!
+ * \brief square::square - конструктор класса square с параметрами
+ * \param ver - хранит в себе координаты вершин квадрата
+ *
+ * - Задает своим полям значения параметров
+ * - Выводит площадь созданной фигуры
+ */
+square::square(double ver[]) {
+    cout << "\nSquare created" << endl;
+
+    for (int i = 0; i < 8; i++) {
+        arr[i] = ver[i];
+    }
+}
+
+/*!
+ * \brief square::square_of_square - высчитывает площадь фигуры
+ * \return result - площадь фигуры типа **double**
+ * \param array - массив с координатами вершин
+ *
+ * - Если фигура не наклонена, считает ее стороны и перемножает их
+ * - Если фигура наклонена, считает диагонали, перемножает их и делит пополам
+ */
+double square::square_of_square(double array[8]) {
+    double result = 0.0;
+
+    if (array[0] == array[6] && array[1] == array[3]) {
+        double x = array[2] - array[0];
+        double y = array[1] - array[7];
+
+        result = x * y;
+    } else {
+        double x1 = array[2] - array[6];
+        double y1 = array[1] - array[5];
+
+        result = (x1 * y1) / 2;
+    }
+
+    return result;
+}
